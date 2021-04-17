e1441990cbd939229540f028c19b66b689786c82
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 01:09:36 +0400

    Create IRoutesHolder.cs

---
+++ SchoolManagementSystem.API/Contracts/IRoutesHolder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SchoolManagementSystem.API.Contracts
+{
+    public interface IRoutesHolder
+    {
+        static string Root { get; set; }
+        static string Version { get; set; }
+        static string Base { get; }
+    }
+}
