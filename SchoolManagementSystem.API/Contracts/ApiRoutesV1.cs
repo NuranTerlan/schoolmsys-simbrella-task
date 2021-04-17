21b8f05cfe97cb4aa6baf799f938b84bdc3818ba
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 01:09:30 +0400

    Create ApiRoutesV1.cs

---
+++ SchoolManagementSystem.API/Contracts/ApiRoutesV1.cs
@@ -0,0 +1,15 @@
+﻿namespace SchoolManagementSystem.API.Contracts
+{
+    public class ApiRoutes : IRoutesHolder
+    {
+        public static string Root { get; set; }
+        public static string Version { get; set; }
+        public static string Base { get; } = Root + "/" + Version;
+
+        public ApiRoutes(string root = "api", string version = "vx")
+        {
+            Root = root;
+            Version = version;
+        }
+    }
+}
