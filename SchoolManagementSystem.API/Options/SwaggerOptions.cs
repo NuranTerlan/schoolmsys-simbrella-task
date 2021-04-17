4c4478b447caa305f91ffc2621cf980bcb606645
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-17 23:56:04 +0400

    no need to include json route prop

--- SchoolManagementSystem.API/Options/SwaggerOptions.cs
+++ SchoolManagementSystem.API/Options/SwaggerOptions.cs
@@ -2,7 +2,6 @@
 {
     public class SwaggerOptions
     {
-        public string JsonRoute { get; set; }
         public string Description { get; set; }
         public string UIEndpoint { get; set; }
     }
