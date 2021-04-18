ceb4bcdfe861ecd90fc483083041ecc8322d466e
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:29:02 +0400

    common application user

---
+++ SchoolManagementSystem.Domain/Commons/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using SchoolManagementSystem.Domain.Enumerations;
+
+namespace SchoolManagementSystem.Domain.Commons
+{
+    public class ApplicationUser : IdentityUser, IAuditInfo
+    {
+        [Required]
+        [MaxLength(255)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string LastName { get; set; }
+        [Required]
+        public short BirthYear { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public Gender Gender { get; set; }
+        [Required]
+        public DateTime CreatedOn { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
