eaa306de0993143f201abfa1b8386e46e6660dec
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:30:34 +0400

    Create 20210418190629_InitialTables-Added.cs

---
+++ SchoolManagementSystem.Infrastructure/Data/Migrations/20210418190629_InitialTables-Added.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolManagementSystem.API.Data.Migrations
+{
+    public partial class InitialTablesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<short>(
+                name: "BirthYear",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedOn",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "AveragePerformanceForYear",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SchoolClassId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AboutMe",
+                table: "AspNetUsers",
+                maxLength: 2000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "CountOfExperienceYears",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "CountOfYearsInThisSchool",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IndividualRoom",
+                table: "AspNetUsers",
+                maxLength: 5,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    LastModifiedOn = table.Column<DateTime>(nullable: true),
+                    Title = table.Column<string>(maxLength: 255, nullable: false),
+                    Description = table.Column<string>(maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SchoolClasses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    LastModifiedOn = table.Column<DateTime>(nullable: true),
+                    Title = table.Column<string>(maxLength: 10, nullable: false),
+                    RoomNumber = table.Column<string>(maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SchoolClasses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeacherClasses",
+                columns: table => new
+                {
+                    TeacherId = table.Column<string>(nullable: false),
+                    SchoolClassId = table.Column<int>(nullable: false),
+                    From = table.Column<DateTime>(nullable: false),
+                    To = table.Column<DateTime>(nullable: false),
+                    CourseId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeacherClasses", x => new { x.TeacherId, x.SchoolClassId });
+                    table.ForeignKey(
+                        name: "FK_TeacherClasses_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TeacherClasses_SchoolClasses_SchoolClassId",
+                        column: x => x.SchoolClassId,
+                        principalTable: "SchoolClasses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TeacherClasses_AspNetUsers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_SchoolClassId",
+                table: "AspNetUsers",
+                column: "SchoolClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherClasses_CourseId",
+                table: "TeacherClasses",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherClasses_SchoolClassId",
+                table: "TeacherClasses",
+                column: "SchoolClassId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_SchoolClasses_SchoolClassId",
+                table: "AspNetUsers",
+                column: "SchoolClassId",
+                principalTable: "SchoolClasses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_SchoolClasses_SchoolClassId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "TeacherClasses");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "SchoolClasses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_SchoolClassId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "BirthYear",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedOn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AveragePerformanceForYear",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "SchoolClassId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AboutMe",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CountOfExperienceYears",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CountOfYearsInThisSchool",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IndividualRoom",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
