22b87b5dc849e095e8dce6b93b679b229470e450
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:29:27 +0400

    Create SchoolClass.cs

---
+++ SchoolManagementSystem.Domain/Entities/SchoolClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using SchoolManagementSystem.Domain.Commons;
+
+namespace SchoolManagementSystem.Domain.Entities
+{
+    public class SchoolClass : AuditEntity
+    {
+        [Required]
+        [MaxLength(10)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string RoomNumber { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+        public virtual ICollection<TeacherClass> TeacherClasses { get; set; }
+    }
+}
