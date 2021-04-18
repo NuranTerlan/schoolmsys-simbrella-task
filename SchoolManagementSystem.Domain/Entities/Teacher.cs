6d1ef0fff65dcf82f2882891dadde90db0d5a0b2
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:29:34 +0400

    Create Teacher.cs

---
+++ SchoolManagementSystem.Domain/Entities/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using SchoolManagementSystem.Domain.Commons;
+using SchoolManagementSystem.Domain.Enumerations;
+
+namespace SchoolManagementSystem.Domain.Entities
+{
+    public class Teacher : ApplicationUser
+    {
+        [MaxLength(2000)]
+        public string AboutMe { get; set; }
+        [Required]
+        [MaxLength(5)]
+        public string IndividualRoom { get; set; }
+        public byte CountOfExperienceYears { get; set; }
+        public byte CountOfYearsInThisSchool { get; set; }
+
+        public virtual ICollection<TeacherClass> TeacherClasses { get; set; }
+    }
+}
