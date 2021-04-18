7e0a4e4f1f9c566cff40da85af98539d5c40feac
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:29:10 +0400

    base entity model

---
+++ SchoolManagementSystem.Domain/Commons/AuditEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagementSystem.Domain.Commons
+{
+    public class AuditEntity : IAuditInfo
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public DateTime CreatedOn { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
