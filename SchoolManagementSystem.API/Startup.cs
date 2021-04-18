bf2a9fa4493a9227e7f7ae9eb760defe2627703c
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:28:41 +0400

    layer specific dependencies are splitted

--- SchoolManagementSystem.API/Startup.cs
+++ SchoolManagementSystem.API/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SchoolManagementSystem.API.Data;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -16,6 +15,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.OpenApi.Models;
 using SchoolManagementSystem.API.Options;
+using SchoolManagementSystem.Infrastructure;
 
 namespace SchoolManagementSystem.API
 {
@@ -31,11 +31,9 @@ namespace SchoolManagementSystem.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddInfrastructure(Configuration);
+
+            services.AddControllersWithViews();
 
             services.AddSwaggerGen(act =>
             {
@@ -86,7 +84,6 @@ namespace SchoolManagementSystem.API
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
             });
         }
     }
