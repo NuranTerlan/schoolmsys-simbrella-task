9bd49f14e32ca0404ce4e239013857c9b3ae169b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:29:23 +0400

    base audit info model interface

---
+++ SchoolManagementSystem.Domain/Commons/IAuditInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SchoolManagementSystem.Domain.Commons
+{
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+        DateTime? LastModifiedOn { get; set; }
+    }
+}
