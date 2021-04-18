587db6191cc256db2074789f9e8b9e5ce4c7e23d
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:29:25 +0400

    Create Course.cs

---
+++ SchoolManagementSystem.Domain/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using SchoolManagementSystem.Domain.Commons;
+
+namespace SchoolManagementSystem.Domain.Entities
+{
+    public class Course : AuditEntity
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        public virtual ICollection<TeacherClass> TeacherClasses { get; set; }
+    }
+}
