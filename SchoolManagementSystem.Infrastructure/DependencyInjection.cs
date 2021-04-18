a2032ad8aab6d5179c30fcc762a2486fff1e0e12
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:31:42 +0400

    infrastructure specific dependencies are injected to app provider services in this layer for invoking in main startup easily, and make it more readable

---
+++ SchoolManagementSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SchoolManagementSystem.Application.Commons.Interfaces;
+using SchoolManagementSystem.Infrastructure.Data;
+
+namespace SchoolManagementSystem.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    optionsBuilder => optionsBuilder
+                        .MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddScoped<IApplicationDbContext>(provider =>
+                provider.GetService<ApplicationDbContext>());
+
+            return services;
+        }
+    }
+}
