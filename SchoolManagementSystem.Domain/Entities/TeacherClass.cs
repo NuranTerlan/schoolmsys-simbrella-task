8979758d8f88b4ee6230ed988685470740a57dae
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:30:03 +0400

    many to many relationship between teacher <-> class <-> subject is implemented

---
+++ SchoolManagementSystem.Domain/Entities/TeacherClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagementSystem.Domain.Entities
+{
+    public class TeacherClass
+    {
+        [Required]
+        public DateTime? From { get; set; }
+        [Required]
+        public DateTime? To { get; set; }
+
+        public string TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+
+        public int SchoolClassId { get; set; }
+        public SchoolClass SchoolClass { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
