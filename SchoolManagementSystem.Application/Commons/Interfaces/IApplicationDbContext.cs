8969a68001ebbb8587456d2daae1e07614eb83e1
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:28:47 +0400

    Create IApplicationDbContext.cs

---
+++ SchoolManagementSystem.Application/Commons/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.Commons.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Course> Courses { get; set; }
+        DbSet<SchoolClass> SchoolClasses { get; set; }
+        DbSet<Student> Students { get; set; }
+        DbSet<Teacher> Teachers { get; set; }
+        DbSet<TeacherClass> TeacherClasses { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
