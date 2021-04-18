4862a863fbe1930ce6d18ed8fc2ad17f5f897a55
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-18 23:29:31 +0400

    Create Student.cs

---
+++ SchoolManagementSystem.Domain/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SchoolManagementSystem.Domain.Commons;
+
+namespace SchoolManagementSystem.Domain.Entities
+{
+    public class Student : ApplicationUser
+    {
+        public byte AveragePerformanceForYear { get; set; }
+
+        [Required]
+        public int? SchoolClassId { get; set; }
+        public virtual SchoolClass SchoolClass { get; set; }
+    }
+}
