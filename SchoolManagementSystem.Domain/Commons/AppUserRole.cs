422dda82c25176ee787411ecd81a55e6f97f4b74
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-19 04:58:32 +0400

    Create AppUserRole.cs

---
+++ SchoolManagementSystem.Domain/Commons/AppUserRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SchoolManagementSystem.Domain.Commons
+{
+    public class AppUserRole : IdentityRole
+    {
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+    }
+}
