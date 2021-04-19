2adf07275eb0db25115dff4855b99172fb658264
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-19 19:58:10 +0400

    Create DependencyInjection.cs

---
+++ SchoolManagementSystem.MainInfrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SchoolManagementSystem.Application.Commons.Interfaces;
+using SchoolManagementSystem.Domain.Commons;
+using SchoolManagementSystem.MainInfrastructure.Data;
+
+namespace SchoolManagementSystem.MainInfrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    optionsBuilder => optionsBuilder
+                        .MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            services.AddIdentity<ApplicationUser, AppUserRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddScoped<IApplicationDbContext>(provider =>
+                provider.GetService<ApplicationDbContext>());
+
+            return services;
+        }
+    }
+}
