729cee22e41ab5e0eb2e447e2f219dda0ba61ba9
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:03:04 +0400

    Create CreateSchoolClassCommand.cs

---
+++ SchoolManagementSystem.Application/SchoolClasses/Commands/CreateSchoolClassCommand.cs
@@ -0,0 +1,11 @@
+﻿using SchoolManagementSystem.Application.SchoolClasses.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.SchoolClasses.Commands
+{
+    public class CreateSchoolClassCommand : IRequestWrapper<SchoolClassDto>
+    {
+        public string Title { get; set; }
+        public string RoomNumber { get; set; }
+    }
+}
