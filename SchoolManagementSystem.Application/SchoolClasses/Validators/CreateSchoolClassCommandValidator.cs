8bb0e34906e2477e55e14a39349f07e12d5fb0ed
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:04:01 +0400

    Create CreateSchoolClassCommandValidator.cs

---
+++ SchoolManagementSystem.Application/SchoolClasses/Validators/CreateSchoolClassCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using SchoolManagementSystem.Application.Courses.Commands;
+using SchoolManagementSystem.Application.SchoolClasses.Commands;
+
+namespace SchoolManagementSystem.Application.SchoolClasses.Validators
+{
+    public class CreateSchoolClassCommandValidator : AbstractValidator<CreateSchoolClassCommand>
+    {
+        public CreateSchoolClassCommandValidator()
+        {
+            RuleFor(c => c.Title).NotEmpty().MinimumLength(3);
+            RuleFor(c => c.RoomNumber).NotEmpty();
+        }
+    }
+}
