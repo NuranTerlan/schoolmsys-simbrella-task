7f7275aa54687b71a0719a05d0990c4fb87729e8
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 10:59:59 +0400

    Create UpdateCourseCommand.cs

---
+++ SchoolManagementSystem.Application/Courses/Commands/UpdateCourseCommand.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagementSystem.Application.Courses.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Courses.Commands
+{
+    public class UpdateCourseCommand : IRequestWrapper<CourseDto>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
