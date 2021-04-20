5936731a0a1009b2b7580192c745892fb2a6ccf1
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:03:52 +0400

    automapper profile is configured for school class entity

---
+++ SchoolManagementSystem.Application/SchoolClasses/MappingProfiles/SchoolClassMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SchoolManagementSystem.Application.SchoolClasses.Commands;
+using SchoolManagementSystem.Application.SchoolClasses.DTOs;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.SchoolClasses.MappingProfiles
+{
+    public class SchoolClassMappingProfile : Profile
+    {
+        public SchoolClassMappingProfile()
+        {
+            CreateMap<SchoolClass, SchoolClassDto>().ReverseMap();
+            CreateMap<CreateSchoolClassCommand, SchoolClass>();
+        }
+    }
+}
