c864f28a0b57853e53348e39351af26efabaeda1
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:01:52 +0400

    Create DeleteCourseCommandValidator.cs

---
+++ SchoolManagementSystem.Application/Courses/Validators/DeleteCourseCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SchoolManagementSystem.Application.Courses.Commands;
+
+namespace SchoolManagementSystem.Application.Courses.Validators
+{
+    public class DeleteCourseCommandValidator : AbstractValidator<DeleteCourseCommand>
+    {
+        public DeleteCourseCommandValidator()
+        {
+            RuleFor(c => c.Id).GreaterThan(0);
+        }
+    }
+}
