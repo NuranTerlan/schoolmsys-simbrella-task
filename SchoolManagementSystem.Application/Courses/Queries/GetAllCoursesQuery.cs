82fedddd4d0924250257bc06896f831524078562
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:01:37 +0400

    Create GetAllCoursesQuery.cs

---
+++ SchoolManagementSystem.Application/Courses/Queries/GetAllCoursesQuery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SchoolManagementSystem.Application.Courses.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Courses.Queries
+{
+    public class GetAllCoursesQuery : IRequestWrapper<IList<CourseDto>>
+    {
+        
+    }
+}
