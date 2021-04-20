37e0a8fa0910362b2e4883310b66291a0d737131
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:01:40 +0400

    Create GetCourseByIdQuery.cs

---
+++ SchoolManagementSystem.Application/Courses/Queries/GetCourseByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using SchoolManagementSystem.Application.Courses.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Courses.Queries
+{
+    public class GetCourseByIdQuery : IRequestWrapper<CourseDto>
+    {
+        public int Id { get; set; }
+    }
+}
