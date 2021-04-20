d302fb8f96b7ee62a5bfc35c4f6b2fd6f7ef8008
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:03:11 +0400

    Create CreateSchoolClassCommandHandler.cs

---
+++ SchoolManagementSystem.Application/SchoolClasses/Handlers/CreateSchoolClassCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using SchoolManagementSystem.Application.Commons.Interfaces;
+using SchoolManagementSystem.Application.SchoolClasses.Commands;
+using SchoolManagementSystem.Application.SchoolClasses.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.SchoolClasses.Handlers
+{
+    public class CreateSchoolClassCommandHandler :
+        IRequestHandlerWrapper<CreateSchoolClassCommand, SchoolClassDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CreateSchoolClassCommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<SchoolClassDto>> Handle(CreateSchoolClassCommand request, CancellationToken cancellationToken)
+        {
+            var newClass = _mapper.Map<SchoolClass>(request);
+            await _context.SchoolClasses.AddAsync(newClass, cancellationToken);
+            var isAdded = await _context.SaveChangesAsync(cancellationToken);
+            if (isAdded == 0)
+            {
+                return Response.Fail<SchoolClassDto>("Couldn't add entity!");
+            }
+
+            var result = _mapper.Map<SchoolClassDto>(newClass);
+            return Response.Success<SchoolClassDto>(result,
+                $"New class with ID#{result.Id} is successfully added to db");
+        }
+    }
+}
