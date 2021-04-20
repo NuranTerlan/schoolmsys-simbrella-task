134e02722f0cfef4052aba5bda3e493c2b661947
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:03:01 +0400

    global response model implemented

---
+++ SchoolManagementSystem.Application/Response.cs
@@ -0,0 +1,25 @@
+﻿namespace SchoolManagementSystem.Application
+{
+    public static class Response
+    {
+        public static Response<T> Success<T>(T data, string message) =>
+            new Response<T>(data, message, false);
+
+        public static Response<T> Fail<T>(string message, T data = default) =>
+            new Response<T>(data, message, true);
+    }
+
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public bool Error { get; set; }
+
+        public Response(T data, string message, bool error)
+        {
+            Data = data;
+            Message = message;
+            Error = error;
+        }
+    }
+}
