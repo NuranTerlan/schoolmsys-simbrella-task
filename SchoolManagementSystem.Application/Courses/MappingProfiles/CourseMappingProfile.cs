d88bdd0f33537b548bee911554239a751e251b57
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:01:07 +0400

    automapper profile is configured for course entity

---
+++ SchoolManagementSystem.Application/Courses/MappingProfiles/CourseMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SchoolManagementSystem.Application.Courses.Commands;
+using SchoolManagementSystem.Application.Courses.DTOs;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.Courses.MappingProfiles
+{
+    public class CourseMappingProfile : Profile
+    {
+        public CourseMappingProfile()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<CreateCourseCommand, Course>();
+        }
+    }
+}
