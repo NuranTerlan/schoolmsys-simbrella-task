575423664f5fbd22ea285ed9581765f6165db9ec
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:04:07 +0400

    Create GetSchoolClassByIdQueryValidator.cs

---
+++ SchoolManagementSystem.Application/SchoolClasses/Validators/GetSchoolClassByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SchoolManagementSystem.Application.SchoolClasses.Queries;
+
+namespace SchoolManagementSystem.Application.SchoolClasses.Validators
+{
+    public class GetSchoolClassByIdQueryValidator : AbstractValidator<GetSchoolClassByIdQuery>
+    {
+        public GetSchoolClassByIdQueryValidator()
+        {
+            RuleFor(c => c.Id).GreaterThan(0);
+        }
+    }
+}
