417bf244ba884dee678b12b92c7f3db1e236e164
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:02:49 +0400

    static dependency injection class is implemented to inject related dependencies in startup as more readable way

---
+++ SchoolManagementSystem.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using SchoolManagementSystem.Application.Commons.Behaviors;
+using SchoolManagementSystem.Application.Courses.Queries;
+
+namespace SchoolManagementSystem.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(GetAllCoursesQuery));
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
