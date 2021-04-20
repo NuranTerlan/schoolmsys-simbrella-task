40ef7a87bf078982af3b111082ba5596f0288822
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:03:56 +0400

    Create GetAllSchoolClassesQuery.cs

---
+++ SchoolManagementSystem.Application/SchoolClasses/Queries/GetAllSchoolClassesQuery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SchoolManagementSystem.Application.SchoolClasses.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.SchoolClasses.Queries
+{
+    public class GetAllSchoolClassesQuery : IRequestWrapper<IList<SchoolClassDto>>
+    {
+        
+    }
+}
