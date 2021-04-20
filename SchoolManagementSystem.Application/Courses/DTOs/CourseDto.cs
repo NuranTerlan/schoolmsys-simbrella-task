5d99eb28777f57d3f8a8435ccc1939f9fd60f42c
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:00:20 +0400

    data transfer object for course is added

---
+++ SchoolManagementSystem.Application/Courses/DTOs/CourseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SchoolManagementSystem.Application.Courses.DTOs
+{
+    public class CourseDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
