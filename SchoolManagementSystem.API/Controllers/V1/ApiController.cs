d5fcce7afa0eed83c17ad93739cc4134f61d5b02
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 10:57:12 +0400

    base custom api controller is implemented

---
+++ SchoolManagementSystem.API/Controllers/V1/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SchoolManagementSystem.API.Controllers.V1
+{
+    public class ApiController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
+    }
+}
