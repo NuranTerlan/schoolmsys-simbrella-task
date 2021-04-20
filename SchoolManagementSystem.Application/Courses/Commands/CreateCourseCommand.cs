ae796a890a577776150ebb12d32a23ff21f189f0
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 10:59:54 +0400

    Create CreateCourseCommand.cs

---
+++ SchoolManagementSystem.Application/Courses/Commands/CreateCourseCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SchoolManagementSystem.Application.Courses.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Courses.Commands
+{
+    public class CreateCourseCommand : IRequestWrapper<CourseDto>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
