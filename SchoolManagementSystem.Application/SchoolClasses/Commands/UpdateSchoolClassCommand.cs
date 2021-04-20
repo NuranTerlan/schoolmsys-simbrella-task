921d3eb1cd445de1ed3117736e14cbb18b6c1c53
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:03:09 +0400

    Create UpdateSchoolClassCommand.cs

---
+++ SchoolManagementSystem.Application/SchoolClasses/Commands/UpdateSchoolClassCommand.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagementSystem.Application.SchoolClasses.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.SchoolClasses.Commands
+{
+    public class UpdateSchoolClassCommand : IRequestWrapper<SchoolClassDto>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string RoomNumber { get; set; }
+    }
+}
