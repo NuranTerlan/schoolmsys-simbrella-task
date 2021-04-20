9063d614e3b4cb02088b2e1a3034958be3575a96
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:03:06 +0400

    Create DeleteSchoolClassCommand.cs

---
+++ SchoolManagementSystem.Application/SchoolClasses/Commands/DeleteSchoolClassCommand.cs
@@ -0,0 +1,9 @@
+﻿using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.SchoolClasses.Commands
+{
+    public class DeleteSchoolClassCommand : IRequestWrapper<int>
+    {
+        public int Id { get; set; }
+    }
+}
