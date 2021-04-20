5af144ebb4cc0beeb9b7b3cf3517cac11624ae72
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:04:45 +0400

    request handler wrapper is created

---
+++ SchoolManagementSystem.Application/Wrappers/IRequestHandlerWrapper.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace SchoolManagementSystem.Application.Wrappers
+{
+    public interface IRequestHandlerWrapper<in TIn, TOut> : IRequestHandler<TIn, Response<TOut>>
+        where TIn : IRequestWrapper<TOut>
+    {
+        
+    }
+}
