c969840f6d155b068aba1f5d42b7bdfe709bb8cb
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:00:42 +0400

    Create GetCourseByIdQueryHandler.cs

---
+++ SchoolManagementSystem.Application/Courses/Handlers/GetCourseByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Application.Commons.Interfaces;
+using SchoolManagementSystem.Application.Courses.DTOs;
+using SchoolManagementSystem.Application.Courses.Queries;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Courses.Handlers
+{
+    public class GetCourseByIdQueryHandler :
+        IRequestHandlerWrapper<GetCourseByIdQuery, CourseDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetCourseByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<CourseDto>> Handle(GetCourseByIdQuery request, CancellationToken cancellationToken)
+        {
+            var course = await _context.Courses.AsNoTracking().SingleOrDefaultAsync(
+                c => c.Id == request.Id, cancellationToken);
+
+            if (course is null)
+            {
+                return Response.Fail<CourseDto>($"Course#{request.Id} isn't found or an error occured while fetching data!");
+            }
+
+            var result = _mapper.Map<CourseDto>(course);
+            return Response.Success<CourseDto>(result, $"Course#{request.Id} is fetched successfully");
+        }
+    }
+}
