4410d4f9a244956750848f732b213d59b22e5f8f
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:00:32 +0400

    data transfer object for school class is added

---
+++ SchoolManagementSystem.Application/SchoolClasses/DTOs/SchoolClassDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.SchoolClasses.DTOs
+{
+    public class SchoolClassDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+        public string Title { get; set; }
+        public string RoomNumber { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
