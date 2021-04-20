180ab49b40dc0b460e6c571a1f0df25a3ffab67b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:01:54 +0400

    Create GetCourseByIdQueryValidator.cs

---
+++ SchoolManagementSystem.Application/Courses/Validators/GetCourseByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SchoolManagementSystem.Application.Courses.Queries;
+
+namespace SchoolManagementSystem.Application.Courses.Validators
+{
+    public class GetCourseByIdQueryValidator : AbstractValidator<GetCourseByIdQuery>
+    {
+        public GetCourseByIdQueryValidator()
+        {
+            RuleFor(c => c.Id).GreaterThan(0);
+        }
+    }
+}
