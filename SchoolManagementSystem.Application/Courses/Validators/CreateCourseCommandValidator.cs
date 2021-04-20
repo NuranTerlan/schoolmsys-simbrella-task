b17dac33eeb1f789d6bcf10a2b09f91a2c4062f9
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:01:48 +0400

    Create CreateCourseCommandValidator.cs

---
+++ SchoolManagementSystem.Application/Courses/Validators/CreateCourseCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SchoolManagementSystem.Application.Courses.Commands;
+
+namespace SchoolManagementSystem.Application.Courses.Validators
+{
+    public class CreateCourseCommandValidator : AbstractValidator<CreateCourseCommand>
+    {
+        public CreateCourseCommandValidator()
+        {
+            RuleFor(c => c.Title).NotEmpty();
+            RuleFor(c => c.Description).NotEmpty();
+        }
+    }
+}
