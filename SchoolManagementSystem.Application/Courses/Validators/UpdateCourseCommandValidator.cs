454454aa31b7b8a17f46561c7b60d6e003c47496
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-20 11:01:57 +0400

    Create UpdateCourseCommandValidator.cs

---
+++ SchoolManagementSystem.Application/Courses/Validators/UpdateCourseCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SchoolManagementSystem.Application.Courses.Commands;
+
+namespace SchoolManagementSystem.Application.Courses.Validators
+{
+    public class UpdateCourseCommandValidator : AbstractValidator<UpdateCourseCommand>
+    {
+        public UpdateCourseCommandValidator()
+        {
+            RuleFor(c => c.Title).NotEmpty();
+            RuleFor(c => c.Description).NotEmpty();
+        }
+    }
+}
