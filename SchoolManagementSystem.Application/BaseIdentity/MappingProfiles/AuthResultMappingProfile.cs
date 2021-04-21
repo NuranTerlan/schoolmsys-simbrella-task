c4038e399dc3439ace7292df27efe6951febf33d
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:57:02 +0400

    Create AuthResultMappingProfile.cs

---
+++ SchoolManagementSystem.Application/BaseIdentity/MappingProfiles/AuthResultMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SchoolManagementSystem.Application.BaseIdentity.DTOs;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.BaseIdentity.MappingProfiles
+{
+    public class AuthResultMappingProfile : Profile
+    {
+        public AuthResultMappingProfile()
+        {
+            CreateMap<AuthenticationResult, AuthResultDto>().ReverseMap();
+        }
+    }
+}
