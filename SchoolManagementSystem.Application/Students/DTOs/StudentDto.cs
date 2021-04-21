a4cb6d32e158928b5595ecdba9ae1d574f92734f
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:57:28 +0400

    Create StudentDto.cs

---
+++ SchoolManagementSystem.Application/Students/DTOs/StudentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolManagementSystem.Application.Students.DTOs
+{
+    public class StudentDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string SchoolClassId { get; set; }
+    }
+}
