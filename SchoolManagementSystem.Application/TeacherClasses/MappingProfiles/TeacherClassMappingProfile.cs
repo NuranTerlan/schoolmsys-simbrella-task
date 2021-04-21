da09848229e89b63be844ec3c8e8e9af982dda28
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:57:46 +0400

    Create TeacherClassMappingProfile.cs

---
+++ SchoolManagementSystem.Application/TeacherClasses/MappingProfiles/TeacherClassMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SchoolManagementSystem.Application.TeacherClasses.Commands;
+using SchoolManagementSystem.Application.TeacherClasses.DTOs;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.TeacherClasses.MappingProfiles
+{
+    public class TeacherClassMappingProfile : Profile
+    {
+        public TeacherClassMappingProfile()
+        {
+            CreateMap<CreateTeacherClassCommand, TeacherClass>();
+            CreateMap<TeacherClass, TeacherClassDto>().ReverseMap();
+        }
+    }
+}
