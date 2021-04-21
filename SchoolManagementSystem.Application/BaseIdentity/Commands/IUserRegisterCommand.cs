b270df05d37651a316e05751888cd2036d958a4b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:55:10 +0400

    base user register command

---
+++ SchoolManagementSystem.Application/BaseIdentity/Commands/IUserRegisterCommand.cs
@@ -0,0 +1,15 @@
+﻿using SchoolManagementSystem.Domain.Enumerations;
+
+namespace SchoolManagementSystem.Application.BaseIdentity.Commands
+{
+    public interface IUserRegisterCommand
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        short BirthYear { get; set; }
+        string Email { get; set; }
+        string Address { get; set; }
+        Gender Gender { get; set; }
+        string Password { get; set; }
+    }
+}
