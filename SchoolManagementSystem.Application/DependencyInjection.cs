db72e73ab8a172d8412af5021b0a00b7ba411c2f
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 20:41:37 +0400

    jwt configuration added

--- SchoolManagementSystem.Application/DependencyInjection.cs
+++ SchoolManagementSystem.Application/DependencyInjection.cs
@@ -1,21 +1,52 @@
 ﻿using System.Reflection;
+using System.Text;
 using FluentValidation;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using SchoolManagementSystem.Application.Commons.Behaviors;
-using SchoolManagementSystem.Application.Courses.Queries;
+using SchoolManagementSystem.Application.Options;
+using SchoolManagementSystem.Application.Students.Commands;
 
 namespace SchoolManagementSystem.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
+        public static IServiceCollection AddApplication(this IServiceCollection services,
+            IConfiguration configuration)
         {
-            services.AddMediatR(typeof(GetAllCoursesQuery));
+            services.AddMediatR(typeof(RegisterStudentCommand));
             services.AddTransient(typeof(IPipelineBehavior<,>),
                 typeof(ValidationBehavior<,>));
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            var jwtSettings = new JwtSettings();
+            configuration.GetSection(nameof(JwtSettings)).Bind(jwtSettings);
+            services.AddSingleton<JwtSettings>(jwtSettings);
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        RequireExpirationTime = false,
+                        ValidateLifetime = true
+                    };
+                });
 
             return services;
         }
