71e3911f9974b6b5f21194fa85d011abdcedfdae
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:54:09 +0400

    inject services to the dependencies container

---
+++ SchoolManagementSystem.API/Installers/MvcInstaller.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace SchoolManagementSystem.API.Installers
+{
+    public static class MvcInstaller
+    {
+        public static IServiceCollection InstallServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "School Management System API",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Nuran Terlan",
+                        Email = "nuran.terlan@mail.ru"
+                    }
+                });
+
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //act.IncludeXmlComments(xmlPath);
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the bearer scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
