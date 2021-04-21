a6e9090549eba8138379d688c14766e7f9720561
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:54:26 +0400

    Create AuthenticationResult.cs

---
+++ SchoolManagementSystem.Domain/Entities/AuthenticationResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SchoolManagementSystem.Domain.Entities
+{
+    public class AuthenticationResult
+    {
+        public string Token { get; set; }
+    }
+}
