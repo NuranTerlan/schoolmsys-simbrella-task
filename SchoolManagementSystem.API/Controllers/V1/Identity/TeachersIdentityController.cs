17f3e568205197cec83dd93cdaedf5079ecc4762
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:53:29 +0400

    Create TeachersIdentityController.cs

---
+++ SchoolManagementSystem.API/Controllers/V1/Identity/TeachersIdentityController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagementSystem.API.Contracts;
+using SchoolManagementSystem.Application.Teachers.Commands;
+
+namespace SchoolManagementSystem.API.Controllers.V1.Identity
+{
+    public class TeachersIdentityController : ApiController
+    {
+        [HttpPost(ApiRoutesV1.Identity.Teacher.Register)]
+        public async Task<IActionResult> Register([FromBody] RegisterTeacherCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPost(ApiRoutesV1.Identity.Teacher.Login)]
+        public async Task<IActionResult> Login([FromBody] LoginTeacherCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
