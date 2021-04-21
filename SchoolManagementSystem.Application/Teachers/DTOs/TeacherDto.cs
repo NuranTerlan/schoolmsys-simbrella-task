d35e9c7e964403e52a0e10ad95f572beb96f71eb
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:57:36 +0400

    Create TeacherDto.cs

---
+++ SchoolManagementSystem.Application/Teachers/DTOs/TeacherDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.Teachers.DTOs
+{
+    public class TeacherDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
