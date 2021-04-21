ac1176888abfd644841995642d2b271cecf745e7
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 20:39:42 +0400

    all action endpoints added for version V1

--- SchoolManagementSystem.API/Contracts/ApiRoutesV1.cs
+++ SchoolManagementSystem.API/Contracts/ApiRoutesV1.cs
@@ -23,5 +23,50 @@
             public const string Update = GetAll + "/{classId}";
             public const string Delete = GetAll + "/{classId}";
         }
+
+        public static class Identity
+        {
+            public const string IdentityBase = Base + "/identity";
+
+            public static class Student
+            {
+                public const string StudentSpecificBase = IdentityBase + "/student";
+
+                public const string Login = StudentSpecificBase + "/login";
+                public const string Register = StudentSpecificBase + "/register";
+            }
+
+            public static class Teacher
+            {
+                public const string TeacherSpecificBase = IdentityBase + "/teacher";
+
+                public const string Login = TeacherSpecificBase + "/login";
+                public const string Register = TeacherSpecificBase + "/register";
+            }
+        }
+
+        public static class Students
+        {
+            public const string GetAll = Base + "/students";
+            public const string GetById = GetAll + "/{studentId}";
+            public const string Create = Base + "/students";
+            public const string Update = GetAll + "/{studentId}";
+            public const string Delete = GetAll + "/{studentId}";
+        }
+
+        public static class Teachers
+        {
+            public const string GetAll = Base + "/teachers";
+            public const string GetById = GetAll + "/{teacherId}";
+            public const string Create = Base + "/teachers";
+            public const string Update = GetAll + "/{teacherId}";
+            public const string Delete = GetAll + "/{teacherId}";
+        }
+
+        public static class TeacherClasses
+        {
+            public const string GetAll = Base + "/teacherclasses";
+            public const string Create = Base + "/teacherclasses";
+        }
     }
 }
