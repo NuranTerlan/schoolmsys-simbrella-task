8f4b3b5b8d7d02b97162e087844c87e745ec1630
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:57:50 +0400

    Create TeacherClassDto.cs

---
+++ SchoolManagementSystem.Application/TeacherClasses/DTOs/TeacherClassDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SchoolManagementSystem.Application.TeacherClasses.DTOs
+{
+    public class TeacherClassDto
+    {
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public string TeacherId { get; set; }
+        public int SchoolClassId { get; set; }
+        public int CourseId { get; set; }
+    }
+}
