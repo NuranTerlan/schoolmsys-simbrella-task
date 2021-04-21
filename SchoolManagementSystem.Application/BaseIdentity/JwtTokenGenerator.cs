1a67148bc2c2a8e4aaa49944d04accd81ae22410
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:56:57 +0400

    token generator class

---
+++ SchoolManagementSystem.Application/BaseIdentity/JwtTokenGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SchoolManagementSystem.Application.BaseIdentity
+{
+    public static class JwtTokenGenerator
+    {
+        public static string GenerateToken(string email, string id, string secret,
+            string role)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, email),
+                    new Claim(ClaimTypes.Role, role), 
+                    new Claim("id", id),
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
