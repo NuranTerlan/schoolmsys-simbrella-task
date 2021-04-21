58a866a9b4d1424efc0c1aec11b25813311be165
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-21 23:57:56 +0400

    Create StudentMappingProfile.cs

---
+++ SchoolManagementSystem.Application/Students/MappingProfiles/StudentMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SchoolManagementSystem.Application.Students.Commands;
+using SchoolManagementSystem.Application.Students.DTOs;
+using SchoolManagementSystem.Domain.Commons;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.Students.MappingProfiles
+{
+    public class StudentMappingProfile : Profile
+    {
+        public StudentMappingProfile()
+        {
+            CreateMap<RegisterStudentCommand, Student>();
+            CreateMap<ApplicationUser, Student>();
+            CreateMap<StudentDto, Student>().ReverseMap();
+        }
+    }
+}
