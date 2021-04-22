e5435b569210d485e852f81f2b012b1959a219ee
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-22 14:57:30 +0400

    Create GetStudentByIdQuery.cs

---
+++ SchoolManagementSystem.Application/Students/Queries/GetStudentByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using SchoolManagementSystem.Application.Students.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Students.Queries
+{
+    public class GetStudentByIdQuery : IRequestWrapper<StudentDto>
+    {
+        public string Id { get; set; }
+    }
+}
