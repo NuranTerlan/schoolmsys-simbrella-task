da5cfc7d580b57feea348c931e7003aea575374b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-22 14:58:26 +0400

    Create GetAllTeachersQuery.cs

---
+++ SchoolManagementSystem.Application/Teachers/Queries/GetAllTeachersQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SchoolManagementSystem.Application.Teachers.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Teachers.Queries
+{
+    public class GetAllTeachersQuery : IRequestWrapper<IList<TeacherDto>>
+    {
+        
+    }
+}
