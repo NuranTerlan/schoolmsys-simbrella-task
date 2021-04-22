86ed8563e377d8968ba51e2a00042a4d848f3476
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-22 14:58:09 +0400

    Create LoginTeacherCommand.cs

---
+++ SchoolManagementSystem.Application/Teachers/Commands/LoginTeacherCommand.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagementSystem.Application.BaseIdentity.Commands;
+using SchoolManagementSystem.Application.BaseIdentity.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Teachers.Commands
+{
+    public class LoginTeacherCommand : IRequestWrapper<AuthResultDto>, IUserLoginCommand
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
