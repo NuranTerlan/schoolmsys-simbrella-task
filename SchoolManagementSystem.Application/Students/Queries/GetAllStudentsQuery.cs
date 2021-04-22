f2d497dd07d7a6b5c02043d624f0b53cfa361f11
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-22 14:57:23 +0400

    Create GetAllStudentsQuery.cs

---
+++ SchoolManagementSystem.Application/Students/Queries/GetAllStudentsQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SchoolManagementSystem.Application.Students.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Students.Queries
+{
+    public class GetAllStudentsQuery : IRequestWrapper<IList<StudentDto>>
+    {
+        
+    }
+}
