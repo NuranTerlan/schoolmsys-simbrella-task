37d6b19ea3b90da23c23cd03c7541aa81460acf7
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-22 14:56:46 +0400

    Create LoginStudentCommand.cs

---
+++ SchoolManagementSystem.Application/Students/Commands/LoginStudentCommand.cs
@@ -0,0 +1,13 @@
+﻿using SchoolManagementSystem.Application.BaseIdentity.Commands;
+using SchoolManagementSystem.Application.BaseIdentity.DTOs;
+using SchoolManagementSystem.Application.Students.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Students.Commands
+{
+    public class LoginStudentCommand : IRequestWrapper<AuthResultDto>, IUserLoginCommand
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
