6c4c8820d2757a58489b778ca8c915c56d011f83
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-22 14:57:04 +0400

    Create GetStudentByIdQueryHandler.cs

---
+++ SchoolManagementSystem.Application/Students/Handlers/GetStudentByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Application.Commons.Interfaces;
+using SchoolManagementSystem.Application.Students.DTOs;
+using SchoolManagementSystem.Application.Students.Queries;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Students.Handlers
+{
+    public class GetStudentByIdQueryHandler :
+        IRequestHandlerWrapper<GetStudentByIdQuery, StudentDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetStudentByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<StudentDto>> Handle(GetStudentByIdQuery request, CancellationToken cancellationToken)
+        {
+            var student = await _context.Students.AsNoTracking()
+                .SingleOrDefaultAsync(s => s.Id == request.Id, cancellationToken);
+            if (student is null)
+            {
+                return Response.Fail<StudentDto>($"Student is not found with ID#{request.Id}");
+            }
+
+            var result = _mapper.Map<StudentDto>(student);
+            return Response.Success<StudentDto>(result, $"Student with {request.Id} ID is fetched");
+        }
+    }
+}
