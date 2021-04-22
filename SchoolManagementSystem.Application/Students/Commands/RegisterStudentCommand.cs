28258c0139d0afcc4a404c6fd5c05fd1e7fefd39
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-22 14:56:49 +0400

    Create RegisterStudentCommand.cs

---
+++ SchoolManagementSystem.Application/Students/Commands/RegisterStudentCommand.cs
@@ -0,0 +1,20 @@
+﻿using SchoolManagementSystem.Application.BaseIdentity.Commands;
+using SchoolManagementSystem.Application.BaseIdentity.DTOs;
+using SchoolManagementSystem.Application.Students.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+using SchoolManagementSystem.Domain.Enumerations;
+
+namespace SchoolManagementSystem.Application.Students.Commands
+{
+    public class RegisterStudentCommand : IRequestWrapper<AuthResultDto>, IUserRegisterCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public short BirthYear { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public Gender Gender { get; set; }
+        public int SchoolClassId { get; set; }
+        public string Password { get; set; }
+    }
+}
