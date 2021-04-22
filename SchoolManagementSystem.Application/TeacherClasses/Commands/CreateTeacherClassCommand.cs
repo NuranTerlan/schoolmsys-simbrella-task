29f2a4216cfd2284ec31616354ddb35758fb6cbe
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-22 14:57:58 +0400

    Create CreateTeacherClassCommand.cs

---
+++ SchoolManagementSystem.Application/TeacherClasses/Commands/CreateTeacherClassCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using SchoolManagementSystem.Application.TeacherClasses.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.TeacherClasses.Commands
+{
+    public class CreateTeacherClassCommand : IRequestWrapper<TeacherClassDto>
+    {
+        public DateTime From { get; set; }
+        public DateTime? To { get; set; }
+        public int SchoolClassId { get; set; }
+        public string TeacherEmail { get; set; }
+        public int CourseId { get; set; }
+    }
+}
