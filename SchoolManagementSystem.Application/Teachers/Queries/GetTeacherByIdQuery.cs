80845bd074db51a0414b9e0ef2e7f4afd430709e
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-22 14:58:28 +0400

    Create GetTeacherByIdQuery.cs

---
+++ SchoolManagementSystem.Application/Teachers/Queries/GetTeacherByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using SchoolManagementSystem.Application.Teachers.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Teachers.Queries
+{
+    public class GetTeacherByIdQuery : IRequestWrapper<TeacherDto>
+    {
+        public string Id { get; set; }
+    }
+}
