dd4b312d445ee21606b5801d925199a5090bf709
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:33:21 +0400

    Create PsychologistsIdentityController.cs

---
+++ SchoolManagementSystem.API/Controllers/V1/Identity/PsychologistsIdentityController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagementSystem.API.Contracts;
+using SchoolManagementSystem.Application.Psychologists.Commands;
+
+namespace SchoolManagementSystem.API.Controllers.V1.Identity
+{
+    public class PsychologistsIdentityController : ApiController
+    {
+        [HttpPost(ApiRoutesV1.Identity.Psychologist.Register)]
+        public async Task<IActionResult> Register([FromBody] RegisterPsychologistCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPost(ApiRoutesV1.Identity.Psychologist.Login)]
+        public async Task<IActionResult> Login([FromBody] LoginPsychologistCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
