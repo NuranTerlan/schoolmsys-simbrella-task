32d1ebd09085da8c1251c83246dbfbab7219c806
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:33:59 +0400

    Create RegisterPsychologistCommand.cs

---
+++ SchoolManagementSystem.Application/Psychologists/Commands/RegisterPsychologistCommand.cs
@@ -0,0 +1,18 @@
+﻿using SchoolManagementSystem.Application.BaseIdentity.Commands;
+using SchoolManagementSystem.Application.BaseIdentity.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+using SchoolManagementSystem.Domain.Enumerations;
+
+namespace SchoolManagementSystem.Application.Psychologists.Commands
+{
+    public class RegisterPsychologistCommand : IRequestWrapper<AuthResultDto>, IUserRegisterCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public short BirthYear { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public Gender Gender { get; set; }
+        public string Password { get; set; }
+    }
+}
