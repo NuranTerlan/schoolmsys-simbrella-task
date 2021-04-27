5697ee919bd7c93413c7efc0a806b734ba162c7d
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:34:28 +0400

    Create GetAllPsychologistsQuery.cs

---
+++ SchoolManagementSystem.Application/Psychologists/Queries/GetAllPsychologistsQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SchoolManagementSystem.Application.Psychologists.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Psychologists.Queries
+{
+    public class GetAllPsychologistsQuery : IRequestWrapper<IList<PsychologistDto>>
+    {
+        
+    }
+}
