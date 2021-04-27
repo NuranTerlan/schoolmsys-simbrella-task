132bd585b6828d2c22de6693cf67fac146084f27
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:34:31 +0400

    Create GetPsychologistByIdQuery.cs

---
+++ SchoolManagementSystem.Application/Psychologists/Queries/GetPsychologistByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using SchoolManagementSystem.Application.Psychologists.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Psychologists.Queries
+{
+    public class GetPsychologistByIdQuery : IRequestWrapper<PsychologistDto>
+    {
+        public string Id { get; set; }
+    }
+}
