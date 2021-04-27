d923f8a3f81c87ef7ba3eb25d5e047fde0913ee6
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:34:38 +0400

    Create GetPsychologistByIdQueryValidator.cs

---
+++ SchoolManagementSystem.Application/Psychologists/Validators/GetPsychologistByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SchoolManagementSystem.Application.Psychologists.Queries;
+
+namespace SchoolManagementSystem.Application.Psychologists.Validators
+{
+    public class GetPsychologistByIdQueryValidator : AbstractValidator<GetPsychologistByIdQuery>
+    {
+        public GetPsychologistByIdQueryValidator()
+        {
+            RuleFor(p => p.Id).NotEmpty();
+        }
+    }
+}
