750baf593d7aa423fb9f4f7348b5a04885276e6b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:34:04 +0400

    Create PsychologistDto.cs

---
+++ SchoolManagementSystem.Application/Psychologists/DTOs/PsychologistDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManagementSystem.Application.Psychologists.DTOs
+{
+    public class PsychologistDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
