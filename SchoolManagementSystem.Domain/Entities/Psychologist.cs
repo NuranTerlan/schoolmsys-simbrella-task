1565d132d7e8f31e1e37bf7b5c64ce9f5efffaec
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:35:07 +0400

    Create Psychologist.cs

---
+++ SchoolManagementSystem.Domain/Entities/Psychologist.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using SchoolManagementSystem.Domain.Commons;
+
+namespace SchoolManagementSystem.Domain.Entities
+{
+    public class Psychologist : ApplicationUser
+    {
+        [MaxLength(5)]
+        public string IndividualRoom { get; set; }
+
+        public virtual ICollection<SchoolClass> SchoolClasses { get; set; }
+    }
+}
