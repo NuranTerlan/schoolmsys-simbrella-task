8e9bdd7102d70fe6a100b2006c7f9de88727c356
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:33:55 +0400

    Create LoginPsychologistCommand.cs

---
+++ SchoolManagementSystem.Application/Psychologists/Commands/LoginPsychologistCommand.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagementSystem.Application.BaseIdentity.Commands;
+using SchoolManagementSystem.Application.BaseIdentity.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Psychologists.Commands
+{
+    public class LoginPsychologistCommand : IRequestWrapper<AuthResultDto>, IUserLoginCommand
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
