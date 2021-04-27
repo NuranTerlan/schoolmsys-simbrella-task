155250919626cd0790540d1fc593e71fe2e47177
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:34:25 +0400

    Create PsychologistMappingProfile.cs

---
+++ SchoolManagementSystem.Application/Psychologists/MappingProfiles/PsychologistMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SchoolManagementSystem.Application.Psychologists.Commands;
+using SchoolManagementSystem.Application.Psychologists.DTOs;
+using SchoolManagementSystem.Domain.Commons;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Application.Psychologists.MappingProfiles
+{
+    public class PsychologistMappingProfile : Profile
+    {
+        public PsychologistMappingProfile()
+        {
+            CreateMap<RegisterPsychologistCommand, Psychologist>();
+            CreateMap<ApplicationUser, Psychologist>();
+            CreateMap<PsychologistDto, Psychologist>().ReverseMap();
+        }
+    }
+}
