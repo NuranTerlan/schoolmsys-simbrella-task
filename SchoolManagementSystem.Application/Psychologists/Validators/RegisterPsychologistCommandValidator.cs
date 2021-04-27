996fd2c3dce5bdf3c4144e38bd8700a36b196647
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-27 11:34:45 +0400

    Create RegisterPsychologistCommandValidator.cs

---
+++ SchoolManagementSystem.Application/Psychologists/Validators/RegisterPsychologistCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using SchoolManagementSystem.Application.BaseIdentity.Validators;
+using SchoolManagementSystem.Application.Psychologists.Commands;
+
+namespace SchoolManagementSystem.Application.Psychologists.Validators
+{
+    public class RegisterPsychologistCommandValidator : RegisterCommandValidator<RegisterPsychologistCommand>
+    {
+        public RegisterPsychologistCommandValidator()
+            :base()
+        {
+            
+        }
+    }
+}
