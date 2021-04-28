36157098bc03a12f57dd985690209bdd3bb75110
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-28 23:55:54 +0400

    Create IncreaseStudentAbsentMarkCommand.cs

---
+++ SchoolManagementSystem.Application/Students/Commands/IncreaseStudentAbsentMarkCommand.cs
@@ -0,0 +1,11 @@
+﻿using SchoolManagementSystem.Application.Students.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Students.Commands
+{
+    public class IncreaseStudentAbsentMarkCommand : IRequestWrapper<StudentDto>
+    {
+        public string StudentId { get; set; }
+        public byte Count { get; set; }
+    }
+}
