c9af6b3497fd4863a0277ada58d309109bdf9c46
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-28 23:56:27 +0400

    Create IncreaseStudentAbsentMarkCommandValidator.cs

---
+++ SchoolManagementSystem.Application/Students/Validators/IncreaseStudentAbsentMarkCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SchoolManagementSystem.Application.Students.Commands;
+
+namespace SchoolManagementSystem.Application.Students.Validators
+{
+    public class IncreaseStudentAbsentMarkCommandValidator : AbstractValidator<IncreaseStudentAbsentMarkCommand>
+    {
+        public IncreaseStudentAbsentMarkCommandValidator()
+        {
+            RuleFor(c => c.StudentId).NotEmpty();
+            RuleFor(c => c.Count).GreaterThan((byte) 0);
+        }
+    }
+}
