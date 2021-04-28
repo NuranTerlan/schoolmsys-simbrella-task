712cb982a09cc9321c464d01718fe847d852e8be
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-28 23:57:20 +0400

    migrations updated

--- SchoolManagementSystem.MainInfrastructure/Migrations/20210428195457_Student-Table-Updated.Designer.cs
+++ SchoolManagementSystem.MainInfrastructure/Migrations/20210428195457_Student-Table-Updated.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolManagementSystem.MainInfrastructure.Data;
 
 namespace SchoolManagementSystem.MainInfrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210428195457_Student-Table-Updated")]
+    partial class StudentTableUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SchoolManagementSystem.MainInfrastructure/Migrations/20210428195457_Student-Table-Updated.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolManagementSystem.MainInfrastructure.Migrations
+{
+    public partial class StudentTableUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "AbsentMarkCount",
+                table: "Students",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "8691d119-f241-474a-bf88-3941618e5caf",
+                columns: new[] { "PasswordHash", "SecurityStamp" },
+                values: new object[] { "AQAAAAEAACcQAAAAEJNXj0Wc59LBvbk0iizILONUXY2JaudYeBmzQT8jxQNPWTy5Hnn1PiNWr5yMavSVjQ==", "ae868f51-56c3-45ff-86aa-24ed488cc9a0" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AbsentMarkCount",
+                table: "Students");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "8691d119-f241-474a-bf88-3941618e5caf",
+                columns: new[] { "PasswordHash", "SecurityStamp" },
+                values: new object[] { "AQAAAAEAACcQAAAAEOHryQF1W9sUqL7tMUkpukOSxOEcqxpnguR0orJav7hr2aJT2mzKSM81/6FVa6ChZA==", "c01455db-f141-4b24-9563-9fac69343c34" });
+        }
+    }
+}
