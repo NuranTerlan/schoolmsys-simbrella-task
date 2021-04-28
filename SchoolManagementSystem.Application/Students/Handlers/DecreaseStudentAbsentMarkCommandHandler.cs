83d3f77ebd855ff602658cd5303c3b68b763618b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-28 23:56:08 +0400

    Create DecreaseStudentAbsentMarkCommandHandler.cs

---
+++ SchoolManagementSystem.Application/Students/Handlers/DecreaseStudentAbsentMarkCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Application.Commons.Interfaces;
+using SchoolManagementSystem.Application.Students.Commands;
+using SchoolManagementSystem.Application.Students.DTOs;
+using SchoolManagementSystem.Application.Wrappers;
+
+namespace SchoolManagementSystem.Application.Students.Handlers
+{
+    public class DecreaseStudentAbsentMarkCommandHandler :
+        IRequestHandlerWrapper<DecreaseStudentAbsentMarkCommand, StudentDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public DecreaseStudentAbsentMarkCommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<StudentDto>> Handle(DecreaseStudentAbsentMarkCommand request, CancellationToken cancellationToken)
+        {
+            var student = await _context.Students.AsNoTracking()
+                .SingleOrDefaultAsync(s => s.Id == request.StudentId,
+                    cancellationToken);
+
+            if (student is null)
+            {
+                return Response.Fail<StudentDto>($"Student with ID#{request.StudentId} doesn't exist!");
+            }
+
+            if (student.AbsentMarkCount - request.Count < 0)
+            {
+                return Response.Fail<StudentDto>("Absent mark can not be negative value!");
+            }
+
+            student.AbsentMarkCount -= request.Count;
+            var result = _mapper.Map<StudentDto>(student);
+            return Response.Success<StudentDto>(result,
+                $"{request.Count} absent mark is deleted from student ID#{request.StudentId} context");
+        }
+    }
+}
